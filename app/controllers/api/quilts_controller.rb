19085d6cbd2260c2849ccc3801637bc11d6abe0d
mikeevans85 <baudecordier@gmail.com>
2018-10-11 19:27:55 -0500

    setting up route, controller, json

--- app/controllers/api/quilts_controller.rb
+++ app/controllers/api/quilts_controller.rb
@@ -1,2 +1,6 @@
 class Api::QuiltsController < ApplicationController
+  def all_quilts_method
+    @quilts = Product.all
+    render "all_quilts.json.jbuilder"
+  end
 end
---
+++ app/views/api/quilts/all_quilts.json.jbuilder
@@ -0,0 +1,1 @@
+json.quilts @quilts
