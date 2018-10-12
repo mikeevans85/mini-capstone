570200e653da54a851baa3feffad5a25594e71c4
mikeevans85 <baudecordier@gmail.com>
2018-10-11 19:14:24 -0500

    run seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quilt1 = Product.new(name: "Blue Quilt", price: 300, size: "55in. x 55in.", image_url: "https://wombatquilts.files.wordpress.com/2014/01/baby-blue-quilt.jpg", description: "This is a blue quilt.")
+quilt1.save
+
+quilt2 = Product.new(name: "Red Quilt", price: 425, size: "46in. x 40in.", image_url: "http://images.allpeoplequilt.mdpcdn.com/sites/allpeoplequilt.com/files/styles/story_detail_enlarge/public/101521596_600w.jpg?itok=94WxyXTZ", description: "This is a red quilt.")
+quilt2.save
+
+quilt3 = Product.new(name: "Green Quilt", price: 275, size: "36in. x 36in.", image_url: "http://static-sympoz.s3.amazonaws.com/blog/2013/03/quilt-pattern-double-irish-chain.jpg", description: "This is a green quilt.")
+quilt3.save
+
+quilt4 = Product.new(name: "Black Quilt", price: 35, size: "2in. x 2in.", image_url: "https://mobileimages.lowes.com/product/converted/038398/038398750153.jpg", description: "This is small.")
+quilt4.save
