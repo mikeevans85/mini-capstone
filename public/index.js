a079f0f6c4fc144caf5fad52af09d9d110da3cb0
mikeevans85 <baudecordier@gmail.com>
2018-11-12 18:46:05 -0600

    Created template for cards and created index.js

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var quiltTemplate = document.querySelector("#QuiltCard");
+var quiltContainer = document.querySelector(".row");
+
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+
