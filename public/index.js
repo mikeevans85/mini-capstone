8c0fb5a600f3f969edd9c908edac1c328afc340f
mikeevans85 <baudecordier@gmail.com>
2018-11-13 17:54:02 -0600

    Updated axios functions

--- public/index.js
+++ public/index.js
@@ -1,10 +1,23 @@
-var quiltTemplate = document.querySelector("#QuiltCard");
-var quiltContainer = document.querySelector(".row");
+/* global axios */
 
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
-quiltContainer.appendChild(quiltTemplate.content.cloneNode(true));
+axios.get("/api/quilts").then(function(response) {
+  var quilts = response.data;
+  console.log("the quilts are", quilts);
 
+  var quiltTemplate = document.querySelector("#QuiltCard");
+  var quiltContainer = document.querySelector(".row");
+
+  quilts.forEach(function(quilt) {
+    var copiedContent = quiltTemplate.content.cloneNode(true);
+    copiedContent.querySelector(".card-title").innerText = quilt.name;
+    copiedContent.querySelector(".card-size").innerText = quilt.size;
+    copiedContent.querySelector(".card-price").innerText = quilt.price;
+    copiedContent.querySelector(".card-text").innerText = quilt.description;
+
+    if (quilt.images[0]) {
+      copiedContent.querySelector(".card-img-top").src = quilt.images[0];
+    }
+
+    quiltContainer.appendChild(copiedContent);
+  });
+});
